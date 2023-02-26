daa9c468ae356c79b2056276a355472f5bd494cf
prateektesting <prateeksahu007@gmail.com>
2023-02-26 11:11:32 +0100

    third commit

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -2,14 +2,10 @@ package Pages;
 
 public class LoginPage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Hello LP");
-		String un = "admin";
-		String pwd = "admin";
-		System.out.println("Logged in");
+	
+	int i=10;
+
 
 	}
 
-}
+
---
+++ src/Pages/RegPage.java
@@ -0,0 +1,15 @@
+package Pages;
+
+public class RegPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello LP");
+		String un = "admin";
+		String pwd = "admin";
+		System.out.println("Logged in");
+
+	}
+
+}
